15774dccf2ca5c1ceed993430a45c8a998e0613c
martinezmp3 <martinez.mp3@gmail.com>
2022-09-24 23:19:02 -0400

    Update Child-MonoPrice-6-Zone-Amp-Controller.groovy

--- Child-MonoPrice-6-Zone-Amp-Controller.groovy
+++ Child-MonoPrice-6-Zone-Amp-Controller.groovy
@@ -1,23 +1,12 @@
 /* 
-Child driver fo Monoprice 6 zone audio 
+child driver fo Monoprice 6 zone audio 
 monoprice.com/product?p_id=10761
 This driver is to control the monoprice 6 zone amplifier. 
 I wrote this diver for personal use. If you decide to use it, do it at your own risk. 
 No guarantee or liability is accepted for damages of any kind. 
-for the driver to work it also needs RS232 to Ethernet like this one:
-        https://www.aliexpress.com/item/32988953549.html?spm=a2g0o.productlist.0.0.517f5e27r8pql4&algo_pvid=f21f7b9e-0d3b-4920-983c-d9df0da59484&algo_expid=f21f7b9e-0d3b-4920-983c-d9df0da59484-1&btsid=0ab6f83115925263810321337e7408&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_
-        https://www.amazon.com/USR-TCP232-302-Serial-Ethernet-Converter-Support/dp/B01GPGPEBM/ref=sr_1_6?dchild=1&keywords=RS232+to+Ethernet&qid=1592526464&sr=8-6 or similar 
-        is been test it too on:
-        https://www.amazon.ca/gp/product/B087J9F6LF/ref=ppx_yo_dt_b_asin_title_o02_s00?ie=UTF8&psc=1
-        08/11/2020
-        this driver also work on a rasberry pi running ser2net.py by Pavel Revak https://github.com/pavelrevak/ser2tcp
-        is recomended to daemonize the scrips instruction on the gihub https://github.com/martinezmp3/Hubitat-Monoprice-6-zone-controller/blob/master/README.md
-        08/12/2020 
-        compatibility with 2 and 3 amps connected as a chain i dont own a second amp
-        08/20/2020 
-        Parent save source name to be display on dashboard
-        08/26/2020
-        add mute override option
+for the driver to work it also needs RS232 to Ethernet like this one 
+https://www.aliexpress.com/item/32988953549.html?spm=a2g0o.productlist.0.0.517f5e27r8pql4&algo_pvid=f21f7b9e-0d3b-4920-983c-d9df0da59484&algo_expid=f21f7b9e-0d3b-4920-983c-d9df0da59484-1&btsid=0ab6f83115925263810321337e7408&ws_ab_test=searchweb0_0,searchweb201602_,searchweb201603_
+https://www.amazon.com/USR-TCP232-302-Serial-Ethernet-Converter-Support/dp/B01GPGPEBM/ref=sr_1_6?dchild=1&keywords=RS232+to+Ethernet&qid=1592526464&sr=8-6
 Jorge Martinez
 */
 metadata {
@@ -48,27 +37,15 @@ metadata {
 	command "nextTrack"
 	command "previousTrack"
     command "toggle"
-//  command "KeypadOff"
     }
 	preferences {
 		section("Device Settings:"){
 				input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
 	    		input name: "Percent",type: "enum", description: "", title: "Percent to dec/enc", options: [[1:1],[2:2],[3:3],[5:5],[10:10],[15:15]], defaultValue: 2
 				input name: "MaxVolumen", type: "NUMBER", description: "", title: "Max volumen allow", defaultValue: 38
-//              input name: "TriggerOn", type: "bool", title: "Any Stting change will trun zone on if zone is off", defaultValue: false
-                input name: "MuteTriggerPower", type: "bool", title: "Mute/unmute command Trigger on/off", defaultValue: false
 			}
 	}
 		
-}
-def KeypadOff(){
-    try{
-//        off ()
-//        On ()
-    }
-    catch (Exception e) {
-        		log.warn "can't ${e.message}"
-    		}
 }
 def play (){
     if (logEnable) log.debug "play"
@@ -81,22 +58,22 @@ def stop (){
     sendEvent(name: "status", value: "stop", isStateChange: true)
 }
 def UpdateData (NewData){
-    log.debug NewData
-def power = NewData.substring(7,9)
+    if (logEnable) log.debug NewData
+	def power = NewData.substring(7,9)
 	if (power.toInteger()){
 		state.switch = "on"
-//        state.status = "on"
-//        sendEvent(name: "status", value: "on", isStateChange: true)
+        state.status = "on"
+        sendEvent(name: "status", value: "on", isStateChange: true)
 		sendEvent(name: "switch", value: "on", isStateChange: true)
 		}
 	if (!power.toInteger()){
 		state.switch = "off"
-//        state.status = "off"
-//        sendEvent(name: "status", value: "off", isStateChange: true)
+        state.status = "off"
+        sendEvent(name: "status", value: "off", isStateChange: true)
     	sendEvent(name: "switch", value: "off", isStateChange: true)
 		}
 	
-def mute = NewData.substring(9,11)
+	def mute = NewData.substring(9,11)
 	if (mute.toInteger()){
 		state.mute = "muted"
 		sendEvent(name: "mute", value: "muted", isStateChange: true)
@@ -105,7 +82,7 @@ def mute = NewData.substring(9,11)
 		state.mute = "unmuted"
 		sendEvent(name: "mute", value: "unmuted", isStateChange: true)
 		}
-def vol = NewData.substring(13,15)
+	def vol = NewData.substring(13,15)
 	if (state.internalLevel != vol){
 		state.internalLevel = vol.toInteger()
 		sendEvent(name: "internalLevel", value: state.internalLevel.toInteger(), isStateChange: true)
@@ -114,24 +91,24 @@ def vol = NewData.substring(13,15)
         state.volume  = state.level
         sendEvent(name: "volume", value: state.volume, isStateChange: true)
 		}
-def source = NewData.substring(21,23)
+	def source = NewData.substring(21,23)
 	if (state.mediaSource != source){
 		state.mediaSource = source.toInteger()
 		sendEvent(name: "mediaSource", value: state.mediaSource.toInteger(), isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource.toInteger())
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
 		}
-def Balance = NewData.substring(19,21)
+	def Balance = NewData.substring(19,21)
 	if (state.balance != Balance){
 		state.balance = Balance.toInteger()
 		sendEvent(name: "balance", value: state.balance.toInteger(), isStateChange: true)
 		}
-def Bass = NewData.substring(17,19)
+	def Bass = NewData.substring(17,19)
 	if (state.bass != Bass){
 		state.bass = Bass.toInteger()
 		sendEvent(name: "bass", value: state.bass.toInteger(), isStateChange: true)
 		}
-def Treble = NewData.substring(15,17)
+	def Treble = NewData.substring(15,17)
 	if (state.treble != Treble){
 		state.treble = Treble.toInteger()
 		sendEvent(name: "treble", value: state.treble.toInteger(), isStateChange: true)
@@ -142,19 +119,19 @@ def setZone (){
 	state.zone = device.deviceNetworkId.substring(len-2,len).toInteger()
 	sendEvent(name: "zone", value: state.zone, isStateChange: true)
 }
+
 def nextTrack(){
-/*    if (settings.TriggerOn && state.switch == "off"){
-        if (logEnable) log.debug "Truning on"
-        on()
-    }*/
-    if(state.mediaSource<6)
+	if(state.mediaSource<6)
 	{
 		def newmediaSource = state.mediaSource+1
-        if (logEnable) log.debug "next Source${state.mediaSource}"
+		if (logEnable) {
+			log.debug "next Source"
+			log.debug state.mediaSource
+			}
 			try {
 			sendEvent(name: "mediaSource", value: (newmediaSource), isStateChange: true)
 			state.mediaSource = newmediaSource
-            state.trackDescription = parent.getChanelName(newmediaSource)
+            state.trackDescription = "Source: ${state.mediaSource}"
             sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
 			parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   			} 
@@ -166,22 +143,21 @@ def nextTrack(){
 def previousTrack(){
 	if(state.mediaSource>1)
 	{
-/*    if (settings.TriggerOn && state.switch == "off"){
-        if (logEnable) log.debug "Truning on"
-        on()
-    }*/
-	def newmediaSource = state.mediaSource-1
-        if (logEnable) log.debug "previous Source${state.mediaSource}"
+		def newmediaSource = state.mediaSource-1
+		if (logEnable) {
+			log.debug "previous Source"
+			log.debug state.mediaSource
+			}
 			try {
-			    sendEvent(name: "mediaSource", value: (newmediaSource), isStateChange: true)
-			    state.mediaSource = newmediaSource
-                state.trackDescription = parent.getChanelName(newmediaSource)
-                sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
-			    parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
+			sendEvent(name: "mediaSource", value: (newmediaSource), isStateChange: true)
+			state.mediaSource = newmediaSource
+            state.trackDescription = "Source: ${state.mediaSource}"
+            sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
+			parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   			} 
 			catch (Exception e) {
         		log.warn "previous Source failed: ${e.message}"
-    		}
+    			}
 	}
 }
 def installed() {
@@ -199,10 +175,6 @@ def initialize(){
 	log.info('MonoPrice 6 Zone Amp Controller: initialize() '+state.name + state.zone)
 }
 def setLevel (Percent){
-/*    if (settings.TriggerOn && state.switch == "off"){
-        if (logEnable) log.debug "Truning on"
-        on()
-    }*/
 	Value = Math.round(Percent/100*settings.MaxVolumen)
     if (logEnable) log.debug ("setLevel call:${Percent}%")
     try {
@@ -229,10 +201,9 @@ def on() {
 	try {
 		state.switch = "on"
         sendEvent(name: "switch", value: "on", isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
-        sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
+        sendEvent(name: "status", value: "on", isStateChange: true)
+        state.status = "on"
 		parent.sendMsg ("<${state.zone}pr01")
-        if (state.mute == muted) unmute()
         }
 	 catch (Exception e) {
         log.warn "Call to on failed: ${e.message}"
@@ -243,6 +214,8 @@ def off() {
     try {
 	 	state.switch = "off"
         sendEvent(name: "switch", value: "off", isStateChange: true) 
+        sendEvent(name: "status", value: "off", isStateChange: true)
+        state.status = "off"
         sendEvent(name: "trackDescription", value: "off", isStateChange: true)
         state.trackDescription = "off"
 		parent.sendMsg ("<${state.zone}pr00")
@@ -301,28 +274,20 @@ def setVolume(Volume) {
 def mute() {
     if (logEnable) log.debug "mute"
     try {
-	    state.mute = "muted"
+	    	state.mute = "muted"
 		sendEvent(name: "mute", value: "muted", isStateChange: true)
-        if (MuteTriggerPower){
-            if (logEnable) log.debug "Overwite mute power off"
-            off()
-        }
-        if (!MuteTriggerPower) parent.sendMsg ("<${state.zone}mu01")
-    } catch (Exception e) {
+	    	parent.sendMsg ("<${state.zone}mu01")
+  } catch (Exception e) {
         log.warn "Call to off failed: ${e.message}"
     }
 }
 def unmute() {
     if (logEnable) log.debug "unmute"
     try {
-	    state.mute = "unmuted"
+	    	state.mute = "unmuted"
 		sendEvent(name: "mute", value: "unmuted", isStateChange: true)
-        if (MuteTriggerPower){
-            if (logEnable) log.debug "Overwite unmute power on"
-            on()
-        }
-        if (!MuteTriggerPower) parent.sendMsg ("<${state.zone}mu00")
-    } catch (Exception e) {
+	    	parent.sendMsg ("<${state.zone}mu00")
+  } catch (Exception e) {
         log.warn "Call to off failed: ${e.message}"
     }
 }
@@ -331,7 +296,7 @@ def Source1(){
     try {
 		state.mediaSource = 1
 		sendEvent(name: "mediaSource", value: 1, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
         parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
     } catch (Exception e) {
@@ -343,7 +308,7 @@ def Source2(){
     try {
 		state.mediaSource = 2
 		sendEvent(name: "mediaSource", value: 2, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
 	    parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   } catch (Exception e) {
@@ -355,7 +320,7 @@ def Source3(){
     try {	    
 		state.mediaSource = 3
 		sendEvent(name: "mediaSource", value: 3, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)        
 		parent.sendMsg ("<${state.zone}ch0${state.mediaSource}") 
   } catch (Exception e) {
@@ -367,7 +332,7 @@ def Source4(){
     try {
 		state.mediaSource = 4
 		sendEvent(name: "mediaSource", value: 4, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)
 	    parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   } catch (Exception e) {
@@ -379,7 +344,7 @@ def Source5(){
     try {	    
 		state.mediaSource = 5
 		sendEvent(name: "mediaSource", value: 5, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)        
 	    parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   } catch (Exception e) {
@@ -391,7 +356,7 @@ def Source6(){
     try {
 		state.mediaSource = 6
 		sendEvent(name: "mediaSource", value: 6, isStateChange: true)
-        state.trackDescription = parent.getChanelName(state.mediaSource)
+        state.trackDescription = "Source: ${state.mediaSource}"
         sendEvent(name: "trackDescription", value: state.trackDescription, isStateChange: true)        
 		parent.sendMsg ("<${state.zone}ch0${state.mediaSource}")
   } catch (Exception e) {
